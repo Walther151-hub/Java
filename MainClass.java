bc0bdb4da25250b644ad3c574a4b1c8b017513ee
Walther <terano@yandex.ru>
2019-10-11 22:49:20 +0300

    Готовое ДЗ Илья Шалыганов задание со звездочкой в отдельном файле

--- MainClass.java
+++ MainClass.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
         // создать переменные всех пройденых типов и инициализировать их значения
@@ -20,8 +22,8 @@ public class MainClass {
 
         // bolean true false
 
-        System.out.println("by="+by+"  \ns="+s+" \ni="+i+" \nl="+l+" \nf="+f+" "+
-                "\ndou="+dou+" \nch="+ch+" \ny="+y+"");
+        System.out.println("byte="+by+"  \nshort="+s+" \nint="+i+" \nlong="+l+" \nfloat="+f+" "+
+                "\ndouble="+dou+" \nchar="+ch+" \nString="+y+"\nbolean true false");
 
         // Написать метод
         // вычисляющий выражение a * (b + (c / d))
@@ -30,29 +32,36 @@ public class MainClass {
 
         System.out.println(printReturnRes(8, 9, 7, 2));
 
+
         System.out.println("Написать метод, принимающий на вход два числа, и проверяющий \n"+
                 "что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, \nв противном случае – false");
 
         System.out.println(twoNumber(7,3));
 
+
         System.out.println("Написать метод, которому в качестве параметра передается целое число, \n" +
                         "метод должен напечатать в консоль положительное ли число передали, или отрицательное");
 
+        System.out.println(oneNumber(5));
 
-        // Работает вне метода
-        // System.out.println(oneNumber(e));
-       // if (e >= 0)
-        //{System.out.print("Число положительное "+e);
-       // }
-       // else {
-        //    System.out.println("Отрицательное число "+e);
-       // }
+
+        System.out.println("Написать метод, которому в качестве параметра передается целое число, \n" +
+                "метод должен вернуть true, если число отрицательное");
+
+        System.out.println(printReturnMinus(-6));
+
+
+        System.out.println("Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод должен \n " +
+                "вывести в консоль сообщение «Привет, указанное_имя!");
+
+                printName("Григорий");
+
 
     }
 
 
     static int printReturnRes(int a, int b, int c, int d) {
-        return a * (b+(c / d));
+         return a * (b+(c / d));
     }
 
     //Написать метод, принимающий на вход два числа, и проверяющий
@@ -61,38 +70,34 @@ public class MainClass {
 
 
     static boolean twoNumber (int k, int h)  {
+        System.out.println("сумма лежит в пределах от 10 до 20");
         return k+h >=10 && k+h <=20;
+
     }
 
    //Написать метод, которому в качестве параметра передается целое число,
     // метод должен напечатать в консоль положительное ли число передали, или
     // отрицательное; Замечание: ноль считаем положительным числом.
+   static int oneNumber (int e) {
+       if (e >= 0) {
+           System.out.print("Число положительное ");
+       } else {
+           System.out.println("Отрицательное число ");
+       }
+       return e;}
+   // }
 
-    // Рекомендация Кирилла
-   // public static void main(String[] args) {
-   //     int e = 5;
-   //     doSmth(e);
-  //  }
+    // Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное;
 
-   // static void doSmth(int num) {
-  //      System.out.println(num);
-    //}
-
-    // Мой вариант метода (не работает)
-  // static void oneNumber (int e) {
-    //if (-e >= 0)
-    // {System.out.print("Число положительное "+e);
-    // }
-    //     else {
-    //    System.out.println("Отрицательное число "+e);
-    // }
-
-    // }
-  //}
+    static boolean printReturnMinus(int r) {
+        System.out.println("Введено отрицательное число");
+        return  r <0;
 
 
-// }
-
+    }
+    static void printName (String name)   {
+        System.out.println("Привет,"+ name +"!!!");
+    }
 
 
 
